9c09e3af4454f2be575f5906322af8a31f0f8865
MyEighthName <58785209+MyEighthName@users.noreply.github.com>
2022-06-15 17:05:29 +0500

    Add files via upload

---
+++ Assets/Scripts/Mechanics/ElementPTInformation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ElementPTInformation
+{
+    public readonly int Number;
+    public readonly string Symbol;
+    public readonly string Name;
+    public readonly string EnglishName;
+    public readonly ElementGroup Group;
+    public readonly float X;
+    public readonly float Y;
+    public Color BackColor => Constants.PTColors[Group].backColor;
+    public Color TextColor => Constants.PTColors[Group].textColor;
+
+    public ElementPTInformation(int number, string symbol, string name, string englishName, ElementGroup group, float x, float y)
+    {
+        Number = number;
+        EnglishName = englishName;
+        Symbol = symbol;
+        Name = name;
+        Group = group;
+        X = x;
+        Y = y;
+    }
+}
